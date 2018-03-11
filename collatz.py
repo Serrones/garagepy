89b78db45994286e8c4d274829a67d958da54e31
serrones <fabioserrones@gmail.com>
2018-03-11 18:19:41 -0300

    Collatz

---
+++ collatz.py
@@ -0,0 +1,21 @@
+"""
+Crie uma função chamada collatz() que tenha um parâmetro de nome number. Se
+number for par, collatz() deverá exibir number // 2 e retornar esse valor. Se
+number for ímpar, collatz() deverá exibir e retornar 3 * number + 1.
+Em seguida crie um programa em que o usuário passe um número qualquer e fique
+chamando collatz até que o resultado seja 1.
+"""
+
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
+        elif number % 2 == 1:
+            number = (3 * number) + 1
+            print(number)
+
+print('Hello mate! Please type any number:\n')
+number = int(input())
+
+collatz(number)
